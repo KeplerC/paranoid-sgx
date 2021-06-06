79cd749a30ff49e7ce1bc8936642d03767505887
Hanming Lu <hanming_lu@berkeley.edu>
2021-06-05 23:27:35 -0400

    update script for system comparison

--- YCSB_traces/proc_trace.py
+++ YCSB_traces/proc_trace.py
@@ -12,41 +12,41 @@ def proc_put(line):
 
 def proc_get(line):
     global M_FINAL_SCRIPT
-    #M_FINAL_SCRIPT += ("\tget(\"{}\");\\\n".format(line[1]))
+    M_FINAL_SCRIPT += ("\tget(\"{}\");\\\n".format(line[1]))
 
 M_FINAL_SCRIPT += "\t LOGD << \"Load started\"; \\\n"
 
-counter = 0
-num_times = 5
-with open("./tracea_load_a.txt") as f:
-    text = f.read() * num_times
-    for line in text.split("\n"):
-        if not line:
-            continue 
-        line = line.split(" ")
-        proc_put(line)
-        counter += 1
+# counter = 0
+# num_times = 5
+# with open("./tracea_load_a.txt") as f:
+#     text = f.read() * num_times
+#     for line in text.split("\n"):
+#         if not line:
+#             continue 
+#         line = line.split(" ")
+#         proc_put(line)
+#         counter += 1
 
-M_FINAL_SCRIPT += "\t LOGD << \"client put end\"; \\\n"
+# M_FINAL_SCRIPT += "\t LOGD << \"client put end\"; \\\n"
 
 # M_FINAL_SCRIPT += "\t LOGD << \"Loaded {} entries\"; \\\n".format(counter)
 
-# counter_put = 0
-# counter_get = 0
-# with open("./tracea_run_a.txt") as f:
-#     text = f.read()
-#     for line in text.split("\n"):
-#         if not line:
-#             continue
-#         line = line.split(" ")
-#         if line[0] == "GET":
-#             proc_get(line)
-#             counter_put += 1
-#         else:
-#             proc_put(line)
-#             counter_get += 1
+counter_put = 0
+counter_get = 0
+with open("./tracea_run_a.txt") as f:
+    text = f.read()
+    for line in text.split("\n"):
+        if not line:
+            continue
+        line = line.split(" ")
+        if line[0] == "GET":
+            proc_get(line)
+            counter_put += 1
+        else:
+            proc_put(line)
+            counter_get += 1
 
-# M_FINAL_SCRIPT += "\t LOGD << \"put {} and get {} end\"; \\\n".format(counter_put, counter_get)
+M_FINAL_SCRIPT += "\t LOGD << \"put {} and get {} end\"; \\\n".format(counter_put, counter_get)
 
 M_FINAL_SCRIPT+= '''}
 
