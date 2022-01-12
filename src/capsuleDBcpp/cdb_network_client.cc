55e72f6ec175cc2b964e2761a443d2a0203abd4f
William Mullen <wmullen@berkeley.edu>
2022-01-12 17:32:47 -0500

    Array bug fixes

--- src/capsuleDBcpp/cdb_network_client.cc
+++ src/capsuleDBcpp/cdb_network_client.cc
@@ -10,11 +10,11 @@
 #include "asylo/platform/primitives/trusted_primitives.h"
 
 
-CapsuleDBNetworkClient::CapsuleDBNetworkClient(size_t blocksize, int id, char[] seed) {
+CapsuleDBNetworkClient::CapsuleDBNetworkClient(size_t blocksize, int id, char seed[]) {
     db = spawnDB(blocksize);
     this->id = id;
 
-    ASYLO_ASSIGN_OR_RETURN(signing_key, EcdsaP256Sha256SigningKey::CreateFromDer(seed));
+    ASYLO_ASSIGN_OR_RETURN(signing_key, asylo::EcdsaP256Sha256SigningKey::CreateFromDer(seed));
     ASYLO_ASSIGN_OR_RETURN(verifying_key, signing_key->GetVerifyingKey());
 }
 
--- src/capsuleDBcpp/cdb_network_client.hh
+++ src/capsuleDBcpp/cdb_network_client.hh
@@ -21,7 +21,7 @@ class CapsuleDBNetworkClient {
         std::unique_ptr <asylo::VerifyingKey> verifying_key;
 
     public:
-        CapsuleDBNetworkClient(size_t blocksize, int id, char[] crypto_param); 
+        CapsuleDBNetworkClient(size_t blocksize, int id, char seed[]); 
         void put(hello_world::CapsulePDU inPDU);
         hello_world::CapsulePDU get(std::string requestedKey);
 };
