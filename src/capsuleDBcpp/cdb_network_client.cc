cfc30147a51ae9dae5d84b0fda753d552447172b
Nivedha Krishnakumar <nivedha.krishnakumar@apple.com>
2022-01-12 09:38:48 -0800

    implemented put for CapsuleDB network

--- src/capsuleDBcpp/cdb_network_client.cc
+++ src/capsuleDBcpp/cdb_network_client.cc
@@ -8,9 +8,21 @@ CapsuleDBNetworkClient::CapsuleDBNetworkClient(size_t blocksize = 50) {
 
 void CapsuleDBNetworkClient::put(hello_world::CapsulePDU inPDU) {
     // Verify hashes
+     if(!asylo::verify_hash(&inPDU)){
+        std::cout << "hash verification failed, not writing to capsuleDB" << endl;
+        return;
+     }
     // Decrypt pdu paylaod (Need to update header file to include key (can just hardcode it))
+    if(asylo::decrypt_payload_l(&inPDU)) {
     // Convert decrypted payload into vector of kvs_payloads
-    // Repeatedly put payloads to db
+       for (std::vector<kvs_payload>::iterator it = inPDU->payload_l.begin() ; it != inPDU->payload_l.end(); it++) {
+            // Repeatedly put payloads to db
+             db.put(&it);
+       }
+    }
+    else
+        std::cout <<"Unable to decrypt payload" << endl;
+    return;
 }
 
 hello_world::CapsulePDU CapsuleDBNetworkClient::get(std::string requestedKey) {
